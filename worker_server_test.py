bdbb2934616988c03644dfde65608ffa1a54f781
kimseongjung <plznw4me@naver.com>
2018-03-20 20:00:58 +0900

    learning cifar using multigpu

--- worker_server_test.py
+++ worker_server_test.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
 
-cluster  = tf.train.ClusterSpec({"worker":["175.211.95.83:5556"] , "ps" : ["127.0.0.1:2222"]})
+cluster  = tf.train.ClusterSpec({"worker":["175.211.95.83:2222"] , "ps" : ["127.0.0.1:2222"]})
 server = tf.train.Server(cluster , job_name= 'worker' , task_index=0)
 server.join()
