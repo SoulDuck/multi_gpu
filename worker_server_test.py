e591e471d67de13b2ab059d6996014b069b3c8d6
kimseongjung <plznw4me@naver.com>
2018-03-20 21:17:05 +0900

    learning cifar using multigpu

--- worker_server_test.py
+++ worker_server_test.py
@@ -2,4 +2,15 @@ import tensorflow as tf
 
 cluster  = tf.train.ClusterSpec({"worker":["192.168.0.16:2222"] , "ps" : ["192.168.0.4:2222"]})
 server = tf.train.Server(cluster , job_name= 'worker' , task_index=0)
-server.join()
+#server.join() #only ps server using join ()method
+print server.target
+#assign ops to the local worker by default
+with tf.device(tf.train.replica_device_setter(worker_device="/job:worker/task:0" , cluster= cluster)):
+    x_ = tf.placeholder(dtype=tf.float32, shape=[1], name='x_')
+    y_ = tf.placeholder(dtype = tf.float32  ,shape=[1] ,name ='y_')
+    a=tf.Variable(3)
+    b=tf.Variable(2)
+    c=a*b
+
+
+
